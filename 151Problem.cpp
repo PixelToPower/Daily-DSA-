41f43593fa4a76f5334b63950a93a9389a3db476
Akshat Panchal <AkshatPanchal0001@outlook.com>
2026-09-13 22:45:42 +0530

    problem 7 solved
    Added functionality to check if a year is a leap year.

--- 151Problem.cpp
+++ 151Problem.cpp
@@ -138,6 +138,27 @@ int main()
     }
 
 
+
+
+// 7.) Check Leap Year
+
+    int year;
+
+    cout << " \nEnter Year : ";
+    cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            cout << year << " is a leap year" << endl;
+        }
+    else
+    {
+        cout << year << " is not a leap year" << endl;
+    }
+
+
+
+    
     
 
     return 0;
